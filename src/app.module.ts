b547a7071696ec691762de3d75551066c7853c2a
Xuan Nguyen <nguyenthxuann@gmail.com>
2025-12-25 17:15:31 +0700

    merge: master + infastruce

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,6 @@
 import { Module } from '@nestjs/common';
-<<<<<<< HEAD
 
 @Module({
   imports: [],
-=======
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
-@Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
->>>>>>> 06862b22a538c3fdd3f96f820998df9121345a9a
 })
-export class AppModule {}
+export class AppModule { }
